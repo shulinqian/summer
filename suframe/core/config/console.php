1e2513b3c18c7b6e4ef5b116a62beddfc684ef25
qianjin <330576744@qq.com>
2019-06-07 11:14:04 +0800

    更改config

--- suframe/core/config/console.php
+++ suframe/core/config/console.php
@@ -11,14 +11,9 @@ EOF;
 return [
 	'name' => 'summer framework',
 	'description' => 'welcome to use summer framework',
-	'debug' => \Inhere\Console\Console::VERB_ERROR,
 	'logoText' => $logo,
 	'logoStyle' => 'fg=green;',
-	'profile' => false,
 	'version' => '0.0.1',
 	'publishAt' => '2019.06.04',
 	'updateAt' => '2019.06.04',
-	'rootPath' => '',
-	'strictMode' => false,
-	'hideRootPath' => true,
 ];
