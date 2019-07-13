1c628984f443de721331d25a03791a945cfeb167
qianjin <330576744@qq.com>
2019-07-14 01:28:39 +0800

    config修改

--- app/config/tcp.php
+++ app/config/tcp.php
@@ -5,6 +5,9 @@ return [
         'port' => 9501,
     ],
     'swoole' => [
-        'worker_num' => 1 //一般为cpu核数的1-4倍
+        'worker_num' => 1, //一般为cpu核数的1-4倍
+        'max_request' => 1000000,
+        'log_file' => __DIR__ . '/../runtime/swoole.log',
+        'pid_file' => __DIR__ . '/../runtime/swoole.pid'
     ]
 ];
